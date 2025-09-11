9d5268b8c26be4569c78efaeb8d42919f8947ea8
bigzone <101182362+appater@users.noreply.github.com>
2025-09-11 15:03:35 +0900

    Title: 정상적인 구구단, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250911/정상적인 구구단/normal-multiple.py
+++ 250911/정상적인 구구단/normal-multiple.py
@@ -1,6 +1,8 @@
 a = int(input())
 
-for i in range(a) : 
-    for j in range(a) : 
-        print(f"{i} * {j} = {i*j}")
+for i in range(1, a+1) : 
+    for j in range(1, a+1) : 
+        print(f"{i} * {j} = {i*j}", end = '')
+        if j != a:
+            print(",", end=' ')
     print()
