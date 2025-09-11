15434cf1ff7ca090e034dc94476f1ee8b158cad9
bigzone <101182362+appater@users.noreply.github.com>
2025-09-11 14:14:34 +0900

    Title: 특정 수 맞히기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250911/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+while num != 25 :
+    if num < 25 :
+        print("Higher")
+        num = int(input())
+    elif num > 25 :
+        print("Lower")
+        num = int(input())
+print("Good")
